6395967ec933a13d7c4e711789e929fa87e6be5c
Kean Leung <keanl@live.unc.edu>
2022-07-22 11:35:04 -0400

    .

--- results/plot.py
+++ results/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-with open('./ppo_molecule_20_002/evaluate/test.pkl', 'rb') as f:
+with open('./ppo_molecule_20_005/evaluate/test.pkl', 'rb') as f:
     data = pickle.load(f)
 
 transitions = data['transitions'].astype(np.float32) - 1.
@@ -12,7 +12,7 @@ transitions = np.mean(transitions, axis=0)
 label = [234, 244, 322, 356, 393, 698, 725, 790, 792, 841, 80, 350, 465, 573, 583, 879, 901, 675, 147, 833]
 
 plt.figure(figsize=(15,7.5))
-plt.title('Molecule 20 Transitions with Cost 0.02 \n Avg. Features Acquired: 16.2, Avg. Accuracy: 0.75')
+plt.title('Molecule 20 Transitions with Cost 0.05 \n Avg. Features Acquired: 2.24, Avg. Accuracy: 0.733')
 plt.bar(list(range(20)), transitions, align='center')
 plt.xticks(list(range(20)), label)
 plt.xlabel('Feature')
