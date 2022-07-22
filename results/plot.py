5b9d05e7c3a1a47d52fc1e9e51994cff6e37255b
Kean Leung <keanl@live.unc.edu>
2022-07-22 11:21:13 -0400

    new results

--- results/plot.py
+++ results/plot.py
@@ -7,16 +7,14 @@ with open('./ppo_molecule_20_005/evaluate/test.pkl', 'rb') as f:
 
 transitions = data['transitions'].astype(np.float32) - 1.
 transitions = np.clip(transitions, 0., None)
-time_steps = data['num_acquisition']
 
-for i in range(transitions.shape[0]):
-    transitions[i] = transitions[i] / time_steps[i]
-
-transitions = np.sum(transitions, axis=0)
+transitions = np.mean(transitions, axis=0)
 label = [234, 244, 322, 356, 393, 698, 725, 790, 792, 841, 80, 350, 465, 573, 583, 879, 901, 675, 147, 833]
 
 plt.figure(figsize=(15,7.5))
 plt.title('Molecule 20 Transitions with Cost 0.05')
 plt.bar(list(range(20)), transitions, align='center')
 plt.xticks(list(range(20)), label)
+plt.xlabel('Feature')
+plt.ylabel('Average Time Steps per Acquisition')
 plt.show()
