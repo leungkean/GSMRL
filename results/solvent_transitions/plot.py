55cfa793d9c08d55757c881681df06b6ab1e3c38
Kean Leung <keanl@live.unc.edu>
2022-07-25 13:04:10 -0400

    Results

--- results/solvent_transitions/plot.py
+++ results/solvent_transitions/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-with open('../ppo_solvent_exp/evaluate/test.pkl', 'rb') as f:
+with open('../ppo_solvent_20_cheap/evaluate/test.pkl', 'rb') as f:
     data = pickle.load(f)
 
 transitions = data['transitions'].astype(np.float32) - 1.
@@ -13,9 +13,9 @@ label_20 = ['1313', '352', '1808', '1594', '1724', '650', '824', '1476', '1379',
 label_exp = ['e1', 't1', 'nn1', 'ne1', 'dipole1', 'ho1', 'lu1', 'holu_gap']
 
 plt.figure(figsize=(15,7.5))
-plt.title('solvent_exp Transitions with Cost 0 \n Reward: -28.1, Num. Acquisition: 7.0')
-plt.bar(list(range(8)), transitions, align='center')
-plt.xticks(list(range(8)), label_exp)
+plt.title('solvent_20_cheap Transitions with Cost 0 \n RMSE: 56.25, Num. Acquisition: 16.0')
+plt.bar(list(range(20)), transitions, align='center')
+plt.xticks(list(range(20)), label_20)
 plt.xlabel('Feature')
 plt.ylabel('Average Time Steps Acquired')
 plt.show()
