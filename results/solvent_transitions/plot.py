1d7bccacde40f3d5d3891101645a4c0dbfb253bf
Kean Leung <keanl@live.unc.edu>
2022-07-28 16:43:46 -0400

    All Results

--- results/solvent_transitions/plot.py
+++ results/solvent_transitions/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 
-with open('../ppo_solvent_20_exp/evaluate/test.pkl', 'rb') as f:
+with open('../ppo_solvent_20_exp_20/evaluate/test.pkl', 'rb') as f:
     data = pickle.load(f)
 
 transitions = data['transitions'].astype(np.float32) - 1.
@@ -13,7 +13,7 @@ label_20 = ['1313', '352', '1808', '1594', '1724', '650', '824', '1476', '1379',
 label_exp = ['e1', 't1', 'nn1', 'ne1', 'dipole1', 'ho1', 'lu1', 'holu_gap']
 
 plt.figure(figsize=(18,9))
-plt.title('solvent_20_exp Transitions with Cheap Cost 0 and Exp Cost 21.75 \n RMSE: 49.12, Num. Acquisition: 5.0')
+plt.title('solvent_20_exp_20 Transitions with Cheap Cost 0 and Exp Cost 20.0 \n RMSE: 37.22, Num. Acquisition: 20.1875')
 plt.bar(list(range(28)), transitions, align='center')
 plt.xticks(list(range(28)), label_20+label_exp)
 plt.xlabel('Feature')
